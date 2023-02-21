c88b7434e9c1f751c289d975f7afb364fe17428c
5salihou <csalikh@gmail.com>
2023-02-21 15:11:01 +0000

    creation des tables EBT creneau et type d'intervention

---
+++ app/Models/type_intervention.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class type_intervention extends Model
+{
+    use HasFactory;
+}
