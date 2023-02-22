5d68e8657e2523c35e0d2331128e853fa00801e7
Papa Ahmadou S Sow <88952482+ahmadous@users.noreply.github.com>
2023-02-22 15:34:07 +0000

    creations des classes et des relations

---
+++ database/migrations/2023_02_22_151335_create_departements_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\filiere;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('departements', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->dateTime('date');
+            $table->timestamps();
+            $table->foreignIdFor(filiere::class)->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('departements');
+    }
+};
