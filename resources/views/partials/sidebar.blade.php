7fd5677afe47a007330caec8402566fcbc6b28c1
Papa Ahmadou S Sow <88952482+ahmadous@users.noreply.github.com>
2023-03-05 19:14:09 +0000

    modification de siidebar dans resources/views/partials

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,15 +1,15 @@
 <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-      <div class="sidebar-brand-icon">
-        <img src="img/logo/logo2.png">
-      </div>
-      <div class="sidebar-brand-text mx-3">Gestion_EDT</div>
+        <div class="sidebar-brand-icon">
+            <img src="img/logo/logo2.png">
+        </div>
+        <div class="sidebar-brand-text mx-3">Gestion_EDT</div>
     </a>
     <hr class="sidebar-divider my-0">
     <li class="nav-item active">
-      <a class="nav-link" href="index.html">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>Dashboard</span></a>
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
     </li>
     {{-- <hr class="sidebar-divider">
     <div class="sidebar-heading">
@@ -72,23 +72,21 @@
       Examples
     </div> --}}
     <li class="nav-item">
-      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePage" aria-expanded="true"
-        aria-controls="collapsePage">
-        <i class="fas fa-fw fa-columns"></i>
-        <span>Pages</span>
-      </a>
-      <div id="collapsePage" class="collapse" aria-labelledby="headingPage" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-          <h6 class="collapse-header">Example Pages</h6>
-          <a class="collapse-item" href="login.html">Login</a>
-          <a class="collapse-item" href="register.html">Register</a>
-          <a class="collapse-item" href="404.html">404 Page</a>
-          <a class="collapse-item" href="blank.html">Blank Page</a>
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePage"
+            aria-expanded="true" aria-controls="collapsePage">
+            <i class="fas fa-fw fa-columns"></i>
+            <span>Pages</span>
+        </a>
+        <div id="collapsePage" class="collapse" aria-labelledby="headingPage" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Example Pages</h6>
+                <a class="collapse-item" href="{{ route('login') }}">{{ __('Login') }}</a>
+                <a class="collapse-item" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </div>
         </div>
-      </div>
     </li>
-   
+
     <hr class="sidebar-divider">
     <div class="version" id="version-ruangadmin"></div>
 
-  </ul>
+</ul>
