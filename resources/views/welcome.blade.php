a41a60de0e07e9d24038cc792085a5cbe92fe7be
Papa Ahmadou S Sow <88952482+ahmadous@users.noreply.github.com>
2023-03-06 23:18:09 +0000

    modification de welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,21 +3,5 @@
 Welcome
 @endsection
 @section('content')
-    <div class="modal" tabindex="-1">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title">welcome</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <p>Bienvenue dans la plateforme Gestion_EDT!</p>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-          </div>
-        </div>
-      </div>
+    <h1>Bienvenue dans la plateforme Gestion_EDT welcome!</h1>
 @endsection
